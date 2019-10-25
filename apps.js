f56b60accc36e685fb5baf8fb153caa7d8a9f472
KinKam Zhao <Kamzh88@KinKams-MacBook-Air.local>
2019-10-24 21:34:56 -0400

    put stuff in document.ready

--- apps.js
+++ apps.js
@@ -1,25 +1,22 @@
 $(document).ready(function () {
-var artist=["Lady Gaga"]
+    var artist = ["Lady Gaga"]
 
-
-
-var queryURL="https://itunes.apple.com/search?term="+ artist;
+    var queryURL = "https://itunes.apple.com/search?term=" + artist;
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
-        console.log(JSON.parse(response));  
+    }).then(function (response) {
+        console.log(JSON.parse(response));
     })
-
+    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword=" + artist + "&apikey=zOsl8qw2cJozfhalFYHMmDpGBYjFaNfr";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log((response));
+    })
 })
 
-    var artist=["Lady Gaga"]
-    var queryURL="https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword="+ artist + "&" + unit +"&apikey=zOsl8qw2cJozfhalFYHMmDpGBYjFaNfr";
-       $.ajax({
-           url: queryURL,
-           method: "GET"
-       }).then(function(response){
-           console.log((response));
-       })
-    })
+
+
 
