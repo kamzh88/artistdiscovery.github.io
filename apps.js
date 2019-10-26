26719048f4d9100e019a983cd8b6a9b3880259ae
KinKam Zhao <Kamzh88@KinKams-MacBook-Air.local>
2019-10-26 12:30:51 -0400

    ticketmaster ajax call

--- apps.js
+++ apps.js
@@ -1,22 +1,77 @@
 $(document).ready(function () {
-    var artist = ["Lady Gaga"]
 
-    var queryURL = "https://itunes.apple.com/search?term=" + artist;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(JSON.parse(response));
+    var tmAuthKey = "zOsl8qw2cJozfhalFYHMmDpGBYjFaNfr";
+    var queryTerm = "";
+    var city = "";
+    var startDate = 0;
+    var endDate = 0;
+    
+
+    //URL Base
+    var queryURLBase ="https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=" + tmAuthKey;
+
+    function runQuery(queryURL) {
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (TMData) {
+            console.log((TMData));
+            console.log(queryURL);
+        })
+
+    }
+
+    $('#submit-btn').on('click', function() {
+        //Get search term
+        event.preventDefault()
+        queryTerm= $("#artist-search").val().trim();
+
+        var newURL = queryURLBase + "&keyword=" + queryTerm;
+
+        runQuery(newURL);
     })
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword=" + artist + "&apikey=zOsl8qw2cJozfhalFYHMmDpGBYjFaNfr";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log((response));
-    })
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // var queryURL = "https://itunes.apple.com/search?term=" + artist;
+
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET"
+        // }).then(function (ITUNESData) {
+        //     console.log(JSON.parse(ITUNESData));
+        // })
+
+        // var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword=" + artist + "&apikey=zOsl8qw2cJozfhalFYHMmDpGBYjFaNfr";
 })
-
 
 
 
